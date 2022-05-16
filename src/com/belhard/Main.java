b51c8b1645dff50fc6c32ffab6d8686c46b78c17
pavel <8707379@mail.ru>
2022-05-16 21:26:00 +0300

    File

--- src/com/belhard/Main.java
+++ src/com/belhard/Main.java
@@ -1,8 +1,62 @@
 package com.belhard;
 
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("f");
+        FileReader fileReader = null;
+        FileWriter fileWriter = null;
+        try {
+            fileReader = new FileReader("file.txt");
+            fileWriter = new FileWriter("fileReaderCopy11.txt");
+            int a ;
+            while ((a = fileReader.read()) != -1) {
+                System.out.print((char) a);
+                fileWriter.write(a);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+    public static void fileByte(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream("file.txt");
+            FileOutputStream fileOutputStream = new FileOutputStream("fileCopy.txt");
+            int a;
+            String name = "";
+            while ((a = fileInputStream.read()) !=-1) {
+                fileOutputStream.write(a);
+//                System.out.print((char) a);
+
+                name += (char) a;
+            }
+            String[] massStr = name.split("\n");
+            for (String str : massStr){
+                String[] nameMass = str.split(" ");
+                System.out.println("Имена: " + nameMass[0]);
+                System.out.println("Фамилия: " + nameMass[1]);
+            }
+//            System.out.println("Имя:" + mass[0]);
+//            System.out.println("Ффмилия:" + mass[1]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
